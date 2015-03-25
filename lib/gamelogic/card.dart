8494ab0d04fd1362f4d6e5aa64eaa4d628886e4f
Brian Grey <mixolyde@gmail.com>
2015-03-24 22:28:32 -0400

    Started card enums and logic

--- lib/gamelogic/card.dart
+++ lib/gamelogic/card.dart
@@ -3,3 +3,28 @@ part of dartless_server;
 enum Character {SCARLET, MUSTARD, WHITE, GREEN, PEACOCK, PLUM}
 enum Room {HALL, KITCHEN}
 enum Weapon {KNIFE}
+
+abstract class Card {}
+
+class Character_Card extends Card {
+  Character character;
+  Character_Card(this.character);
+}
+
+class Room_Card extends Card {
+  Room room;
+  Room_Card(this.room);
+}
+
+class Weapon_Card extends Card {
+  Weapon weapon;
+  Weapon_Card(this.weapon);
+}
+
+List<Card> newDeck(){
+  List<Card> cards = [];
+  List<Room> rooms = Room.values;
+  
+  
+  return cards;
+}
