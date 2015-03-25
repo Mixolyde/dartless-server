9a2d82cdf8b4b3f725e74891f012bc1b90efc42a
Brian Grey <mixolyde@gmail.com>
2015-03-25 16:50:25 -0400

    Added board classes for locations

--- lib/gamelogic/board.dart
+++ lib/gamelogic/board.dart
@@ -1,1 +1,20 @@
 part of dartless_server;
+
+abstract class BoardLocation {}
+
+class Hallway extends BoardLocation {
+  Room room1;
+  Room room2;
+  Hallway(this.room1, this.room2);
+}
+
+class Board_Room extends BoardLocation {
+  Room room;
+  Board_Room(this.room);
+}
+
+class Passage{
+  Room room1;
+  Room room2;
+  Passage(this.room1, this.room2);
+}
