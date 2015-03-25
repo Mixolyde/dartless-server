a03889126f1b2c255995b99874a37da2d35b6199
Brian Grey <mixolyde@gmail.com>
2015-03-25 17:23:03 -0400

    Basic game data classes

--- lib/gamelogic/game_data.dart
+++ lib/gamelogic/game_data.dart
@@ -1,1 +1,31 @@
 part of dartless_server;
+
+class GameData {
+  var players = new Map<Character, PlayerData>();
+  String winner = null;
+  MoveState moveState = null;
+  
+  GameData(this.players, this.moveState);
+}
+
+var players: Map[Character, PlayerData] = Map()
+var winner: Option[String] = None
+var moveState: Option[MoveState] = None
+
+
+case class PlayerData {
+  final String name;
+  final Set<Card> hand;
+  
+  BoardLocation boardLocation;
+  
+  PlayerData(this.name, this.hand, this.boardLocation);
+}
+
+enum MovePhase {MOVE, SUGGESTION, ACCUSATION, CHOOSE_CARD}
+
+class MoveState {
+  Character character;
+  MovePhase phase;
+  MoveState(this.character, this.phase);
+}
