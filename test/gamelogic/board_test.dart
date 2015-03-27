bde859681506c58b30b39d912161c7bc78fa0515
Brian Grey <mixolyde@gmail.com>
2015-03-27 17:32:02 -0400

    Adding static data board tests

--- test/gamelogic/board_test.dart
+++ test/gamelogic/board_test.dart
@@ -1,1 +1,19 @@
 library board_test;
+
+import 'package:unittest/unittest.dart';
+import 'package:dartless_server/dartless_server.dart';
+void main() {
+  group('board static data', () {
+    test('contains passage', () {
+      expect(Board.ALL_PASSAGES, contains(Passage(Board_Room.STUDY, Board_Room.KITCHEN)));
+      expect(Board.ALL_PASSAGES, contains(Passage(Board_Room.CONSERVATORY, Board_Room.LOUNGE)));
+    });
+    test('contains hallways', () {
+      expect(Board.ALL_HALLWAYS, contains(Hallway(Board_Room.STUDY, Board_Room.HALL)));
+      expect(Board.ALL_HALLWAYS, contains(Hallway(Board_Room.HALL, Board_Room.LOUNGE)));
+    });
+    test('Scarlet starts in the hall/lounge hallway', () {
+      expect(Board.getStart(Character.SCARLET), equals(Hallway(Board_Room.HALL, Board_Room.LOUNGE)));
+    });
+  });
+}
