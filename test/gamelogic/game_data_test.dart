993edcc7dd6a84dc1faa113c6d8f1ef67fc64897
Brian Grey <mixolyde@gmail.com>
2015-03-31 17:09:32 -0400

    Fixed named constructor call

--- test/gamelogic/game_data_test.dart
+++ test/gamelogic/game_data_test.dart
@@ -6,7 +6,7 @@ import 'package:dartless_server/dartless_server.dart';
 void main() {
   group('new game data tests', () {
     test('new game data values', () {
-      var data = GameData.newGame();
+      var data = new GameData.newGame();
       
       expect(data.state, equals(GameState.NEW));
       expect(data.players.keys.length, equals(0));
