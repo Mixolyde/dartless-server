bf9338f79fd0dc143cb8900071eb545a025f166d
Brian Grey <mixolyde@gmail.com>
2015-03-31 17:02:51 -0400

    Initial game data test

--- test/gamelogic/game_data_test.dart
+++ test/gamelogic/game_data_test.dart
@@ -1,1 +1,15 @@
 library game_data_test;
+
+void main() {
+  group('new game data tests', () {
+    test('new game data values', () {
+      var data = GameData.newGame();
+      
+      expect(data.state, equals(GameState.NEW));
+      expect(data.players.keys.length, equals(0));
+      expect(data.winner, isNull);
+      expect(data.moveState, isNull);
+    });
+  });
+
+}
