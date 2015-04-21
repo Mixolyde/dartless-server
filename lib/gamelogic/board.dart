bc7d8da4e7e128e0bfa2a73d0097eb14142b0548
Brian Grey <mixolyde@gmail.com>
2015-04-21 11:35:58 -0400

    Started adding X, Y values to board locations

--- lib/gamelogic/board.dart
+++ lib/gamelogic/board.dart
@@ -16,36 +16,38 @@ part of dartless_server;
 // Passages connect the corner Board_Rooms, but are not locations on the board
 
 abstract class BoardLocation {
-  const BoardLocation();
+  final int x;
+  final int y;
+  const BoardLocation(this.x, this.y);
 }
 
 class Hallway extends BoardLocation {
   final Board_Room room1;
   final Board_Room room2;
-  const Hallway(this.room1, this.room2);
+  const Hallway(x, y, this.room1, this.room2):super.(x, y);
 }
 
 class Board_Room extends BoardLocation {
   final Room room;
-  const Board_Room._(this.room);
+  const Board_Room._(x, y, this.room):super.(x, y);
   static const Board_Room HALL =
-    const Board_Room._(Room.HALL);
+    const Board_Room._(1, 1, Room.HALL);
   static const Board_Room STUDY =
-    const Board_Room._(Room.STUDY);
+    const Board_Room._(1, 1, Room.STUDY);
   static const Board_Room LIBRARY =
-    const Board_Room._(Room.LIBRARY);
+    const Board_Room._(1, 1, Room.LIBRARY);
   static const Board_Room CONSERVATORY =
-    const Board_Room._(Room.CONSERVATORY);
+    const Board_Room._(1, 1, Room.CONSERVATORY);
   static const Board_Room BALL_ROOM =
-    const Board_Room._(Room.BALL_ROOM);
+    const Board_Room._(1, 1, Room.BALL_ROOM);
   static const Board_Room BILLIARD_ROOM =
-    const Board_Room._(Room.BILLIARD_ROOM);
+    const Board_Room._(1, 1, Room.BILLIARD_ROOM);
   static const Board_Room LOUNGE =
-    const Board_Room._(Room.LOUNGE);
+    const Board_Room._(1, 1, Room.LOUNGE);
   static const Board_Room DINING_ROOM =
-    const Board_Room._(Room.DINING_ROOM);
+    const Board_Room._(1, 1, Room.DINING_ROOM);
   static const Board_Room KITCHEN =
-    const Board_Room._(Room.KITCHEN);    
+    const Board_Room._(1, 1, Room.KITCHEN);    
 }
 
 class Passage{
@@ -71,18 +73,18 @@ abstract class Board {
     const Passage(Board_Room.STUDY, Board_Room.KITCHEN),
     const Passage(Board_Room.CONSERVATORY, Board_Room.LOUNGE)];
     
-  static const List<Passage> ALL_HALLWAYS = const [
-    const Hallway(Board_Room.STUDY, Board_Room.HALL),
-    const Hallway(Board_Room.HALL, Board_Room.LOUNGE)];
+  static const List<Hallway> ALL_HALLWAYS = const [
+    const Hallway(2, 1, Board_Room.STUDY, Board_Room.HALL),
+    const Hallway(4, 1, Board_Room.HALL, Board_Room.LOUNGE)];
     
   //starting locations
   static const Map<Character, Hallway> START_LOCATIONS = const {
-    Character.SCARLET : const Hallway(Board_Room.HALL, Board_Room.LOUNGE),
-    Character.MUSTARD : const Hallway(Board_Room.LOUNGE, Board_Room.DINING_ROOM),
-    Character.WHITE   : const Hallway(Board_Room.BALL_ROOM, Board_Room.KITCHEN),
-    Character.GREEN   : const Hallway(Board_Room.CONSERVATORY, Board_Room.BALL_ROOM),
-    Character.PEACOCK : const Hallway(Board_Room.LIBRARY, Board_Room.CONSERVATORY),
-    Character.PLUM    : const Hallway(Board_Room.STUDY, Board_Room.LIBRARY)
+    Character.SCARLET : ALL_HALLWAYS[2],
+    Character.MUSTARD : const Hallway(2, 1, Board_Room.LOUNGE, Board_Room.DINING_ROOM),
+    Character.WHITE   : const Hallway(2, 1, Board_Room.BALL_ROOM, Board_Room.KITCHEN),
+    Character.GREEN   : const Hallway(2, 1, Board_Room.CONSERVATORY, Board_Room.BALL_ROOM),
+    Character.PEACOCK : const Hallway(2, 1, Board_Room.LIBRARY, Board_Room.CONSERVATORY),
+    Character.PLUM    : const Hallway(2, 1, Board_Room.STUDY, Board_Room.LIBRARY)
   };
   
   static Hallway getStart(Character char) => START_LOCATIONS[char];
